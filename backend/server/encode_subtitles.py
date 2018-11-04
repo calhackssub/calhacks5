7938b1d3f1deb8fb5d57df9e8c9521ecf42b1825
Austin Do <austincqdo@berkeley.edu>
2018-11-04 08:01:17 -0800

    final commit

--- backend/server/encode_subtitles.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def subtitleVideo(name):
-	command = "ffmpeg -i " + name + ".mp4" + " -vf subtitles=" + name + ".srt " + name + "-subbed.mp4"
-	subprocess.call(command, shell=True)
